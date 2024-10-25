3edfc8a0a704c8c702c3dea5c6dc49e556085a8b
Ivan Buzin <ua.buzin@gmaile.com>
2024-10-25 19:13:35 +0300

    nev

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,7 @@ const pug = require("gulp-pug");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 
-function serve() {
-  browserSync.init({
-    server: "./Compiled-HTML",
-    open: false, // Додаємо цю опцію, щоб уникнути автоматичного відкриття браузера
-  });
-}
-
 // Компіляція Pug
-
 function compilePug() {
   return gulp
     .src("src/pug/**/*.pug")
@@ -39,16 +31,24 @@ function compileScss() {
     .pipe(browserSync.stream());
 }
 
+// Копіювання зображень
+gulp.task("images", function () {
+  return gulp.src("src/img/**/*").pipe(gulp.dest("Compiled-HTML/img"));
+});
+
 // Live Server для автоматичного оновлення браузера
 function serve() {
   browserSync.init({
     server: {
-      baseDir: "./dist",
+      baseDir: "./Compiled-HTML",
     },
   });
   gulp.watch("src/pug/**/*.pug", compilePug);
-  gulp.watch("src/scss/**/*.scss", compileScss);
-  gulp.watch("dist/*.html").on("change", browserSync.reload);
+  gulp.watch("src/scss/**/*.scss", gulp.series(compileScss));
+  gulp
+    .watch("src/img/**/*", gulp.series("images"))
+    .on("change", browserSync.reload);
+  gulp.watch("./Compiled-HTML/*.html").on("change", browserSync.reload);
 }
 
-exports.default = gulp.series(compilePug, compileScss, serve);
+exports.default = gulp.series(compilePug, compileScss, "images", serve);
