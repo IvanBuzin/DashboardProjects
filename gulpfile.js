01745f564621cce587c0f93095794bf09d05a734
Ivan Buzin <ua.buzin@gmaile.com>
2024-10-25 12:05:44 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -34,28 +34,3 @@ function serve() {
 }
 
 exports.default = gulp.series(compilePug, compileScss, serve);
-
-// const gulp = require("gulp");
-// const pug = require("gulp-pug");
-// const browserSync = require("browser-sync").create();
-
-// gulp.task("pug", function () {
-//   return gulp
-//     .src("src/pug/**/*.pug")
-//     .pipe(pug({ pretty: true }))
-//     .pipe(gulp.dest("dist"))
-//     .pipe(browserSync.stream());
-// });
-
-// gulp.task("serve", function () {
-//   browserSync.init({
-//     server: {
-//       baseDir: "dist",
-//     },
-//   });
-
-//   gulp.watch("src/pug/**/*.pug", gulp.series("pug"));
-//   gulp.watch("dist/*.html").on("change", browserSync.reload);
-// });
-
-// gulp.task("default", gulp.series("pug", "serve"));
