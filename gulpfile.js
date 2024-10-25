50e3e02afbbb082244f41b82ed5c5eec2962770a
Ivan Buzin <ua.buzin@gmaile.com>
2024-10-25 14:11:17 +0300

    nr

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,29 @@ const pug = require("gulp-pug");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 
+function serve() {
+  browserSync.init({
+    server: "./Compiled-HTML",
+    open: false, // Додаємо цю опцію, щоб уникнути автоматичного відкриття браузера
+  });
+}
+
 // Компіляція Pug
+
 function compilePug() {
   return gulp
     .src("src/pug/**/*.pug")
-    .pipe(pug({ pretty: true }))
+    .pipe(
+      pug({
+        pretty: true,
+        locals: {
+          data: [
+            { id: 1, name: "Jane Cooper", email: "jane@microsoft.com" },
+            { id: 2, name: "Floyd Miles", email: "floyd@yahoo.com" },
+          ],
+        },
+      })
+    )
     .pipe(gulp.dest("Compiled-HTML"))
     .pipe(browserSync.stream());
 }
